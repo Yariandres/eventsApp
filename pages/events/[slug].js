fa6ee68be523f5af6a2fe01644ca3ae8dbf3f506
Yari Herrera <yari.herrera@valtech.com>
2022-08-14 03:43:11 +0200

    adds css files for event page

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,10 +1,57 @@
+import { FaPencilAlt, FaTimes } from "react-icons/fa";
 import Layout from "@/components/Layout";
+import Link from "next/link";
+import Image from "next/image";
 import { API_URL } from "@/config/index";
+import styles from "@/styles/Event.module.css";
 
 export default function EventPage({ evt }) {
+  const deleteEvent = async id => {
+    const res = await fetch(`${API_URL}/api/events/${id}`, {
+      method: "DELETE",
+    });
+    const data = await res.json();
+    console.log(data);
+  };
+
   return (
     <Layout>
-      <h1>{evt.name}</h1>
+      <div className={styles.event}>
+        <div className={styles.controls}>
+          <Link href={`/events/edit/${evt.id}`}>
+            <a>Edit Event</a>
+          </Link>
+          <a href="#" className={styles.delete} onClick={deleteEvent}>
+            <FaTimes /> Delete Event
+          </a>
+        </div>
+        <span>
+          {evt.date} at {evt.time}
+        </span>
+        <h1>{evt.name}</h1>
+        {evt.image && (
+          <div className={styles.image}>
+            <Image
+              src={evt.image}
+              width={960}
+              height={600}
+              alt="event image"
+            ></Image>
+          </div>
+        )}
+        <h3>Performerse:</h3>
+        <p>{evt.performers}</p>
+        <h3>Description</h3>
+        <p>{evt.description}</p>
+        <h3>Venue: {evt.venue}</h3>
+        <p>{evt.address}</p>
+
+        <Link href="/events">
+          <a className={styles.back}>
+            {'<'} Go back to events
+          </a>
+        </Link>
+      </div>
     </Layout>
   );
 }
